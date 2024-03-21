1eded7028c59c657f7a7f605f83e15a2aa1fce9a
Mikhail_Biziukov <HostProcessor@gmail.com>
2024-03-21 18:00:14 +0700

    deleted some foo

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import controller.Controller;
 import data.Student;
 import data.Teacher;
-import service.DataService;
 import view.StudentView;
 import view.TeacherView;
 
@@ -11,13 +10,7 @@ public class Main {
         Student student2 = new Student("Петров Петр Петрович", 1995, "Красноярск",0);
         Teacher teacher1 = new Teacher("Семенов Семен Семенович", 1980, "Питер",0);
         Teacher teacher2 = new Teacher("Янковский Иван Олегович", 1975, "Москва",0);
-//        DataService dataService = new DataService();
-//        dataService.createUser(student1);
-//        dataService.createUser(student2);
-//        dataService.createUser(teacher1);
-//        dataService.createUser(teacher2);
-//        System.out.println(dataService.readUser("Teacher"));
-//        System.out.println(dataService.readUser("Student"));
+
         Controller controller = new Controller();
         controller.create(student1);
         controller.create(student2);
